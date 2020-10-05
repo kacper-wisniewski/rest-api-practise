9313dfa944c2f0afadbda50388caa800ff0921c3
Kacper Wiśniewski <kaka1341@gmail.com>
2020-10-05 16:35:44 +0200

    Add .gitignore file

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-// const {v4: uuidv4} = require('uuid');
-//const db = require('./db.js');
-// const testimonialsRoutes = require('./routes/testimonials.routes');
-
+const cors = require('cors');
 const app = express();
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
+app.use(
+  cors()
+);
 
 app.use(
   express.urlencoded( {extended: 'true'} )
